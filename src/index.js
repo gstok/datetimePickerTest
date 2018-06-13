edc81e099c1347468bc5f721df11d9f85b6bca54
顾世豪 <gushihao@worken.cn>
2018-06-13 19:39:05 +0800

    下班走了

--- src/index.js
+++ src/index.js
@@ -6,5 +6,12 @@ function getMonths (year) {
     return Array(12).fill(0).map((item, index) => index + 1);
 }
 
+function getDays (year, month) {
+    
+}
+
+
+let date = new Date(2005, 12, 0);
+console.log(date.getDate());
 
 console.log(getMonths(1992));
